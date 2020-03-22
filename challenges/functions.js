f48205f9faa92e49d9cd34c6e684e26dfdd1e6a1
Johnjohnn <conde2010@aol.com>
2020-03-22 19:52:19 -0400

    finished

--- challenges/functions.js
+++ challenges/functions.js
@@ -31,22 +31,22 @@ return 'Hello ${first_name} ${last_name}, nice to meet you!'
 }
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-//consume(2,2,add);  4
- //consume(10,16,multiply);  160
- //consume("Mary","Poppins", greeting); //Hello Mary Poppins, nice to meet you!
+consume(2,2,add);  4
+ consume(10,16,multiply);  160
+ consume("Mary","Poppins", greeting); //Hello Mary Poppins, nice to meet you!
 
 
 
- //console.log(consume(2, 2, add));  4
- //console.log(consume(10, 16, multiply));  160
- //console.log(consume("Mary", "Poppins", greeting)); //Hello Mary Poppins, nice to meet you!
+ console.log(consume(2, 2, add));  4
+ console.log(consume(10, 16, multiply));  160
+ console.log(consume("Mary", "Poppins", greeting)); //Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: A nested function can access other local functions , variables , costants , types etc because they are functions nested within other functions and this is called lexical scoping .
 
 
 const external = "I'm outside the function";
